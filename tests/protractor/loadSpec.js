9df35dd524273f56ad808c415048357a2cd9c894
toddb <todd@goneopen.com>
2013-10-30 09:35:44 +1300

    New protractor syntax from 0.12.0

--- tests/protractor/loadSpec.js
+++ tests/protractor/loadSpec.js
@@ -1,20 +1,15 @@
 describe('Test Application - async loader', function () {
-  var ptor;
   beforeEach(function () {
-    ptor = protractor.getInstance();
-    ptor.get('/');
+    browser.get('/');
   });
 
   it('should display text', function () {
-    var greeting = ptor.findElement(
-        protractor.By.css("body>div"));
-
+    var greeting = element(by.css("body>div"));
     expect(greeting.getText()).toContain("test app: v");
   });
 
   it('should show version', function () {
-    var greeting = ptor.findElement(
-        protractor.By.css("body>div"));
+    var greeting = element(by.css("body>div"));
     expect(greeting.getText()).toContain("0.1");
   });
 });
